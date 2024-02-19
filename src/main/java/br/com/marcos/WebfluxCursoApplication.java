8bf77ca095bccfa3fe12ebbd3fa8637c69d506a8
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-02-19 11:01:12 -0300

    first commit

---
+++ src/main/java/br/com/marcos/WebfluxCursoApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxCursoApplication.class, args);
+	}
+
+}
